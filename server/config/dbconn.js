e4469768ebef2591be868683efcc545c20571eb4
Your Name <you@example.com>
2023-11-14 13:14:32 +0700

    khoitaoexpress_and ketnoidb

---
+++ server/config/dbconn.js
@@ -0,0 +1,24 @@
+const { Sequelize } = require("sequelize");
+//tạo hàm để kết nối với database
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.BD_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+    logging : false,   // bỏ những dòng log mặc định không cần thiết
+    timezone:"+07:00",  // múi giờ 
+  }
+);
+
+const dbconn = async()=>{
+    try {
+        await sequelize.authenticate();  // Nêú kết nối database thành công thì loggin
+        console.log('DB CONNECTED!');
+      } catch (error) {
+        console.error('Unable to connect to the database:', error); // nếu kết nối không thành công thì bắn error
+      }
+}
+
+module.exports = dbconn
