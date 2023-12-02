17d2556abb57fad7ab47a03bd96d7c312ecaebff
Your Name <you@example.com>
2023-12-02 13:39:18 +0700

    khoitaoclient

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+
+
+  return (
+    <h1 className="text-red-300 font-extrabold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
